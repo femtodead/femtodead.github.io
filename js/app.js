378de2654cc3ca7b8d7c1de77a05af17edda8291
Александр <femt0dead3@mail.ru>
2023-05-30 00:34:24 +0300

    js-job

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 
 
-function inputcom() {
-    x = parseInt(document.getElementById("coment").value)
-    arr.push(x);
-    document.getElementById("result").innerText = "sdfsdf";
+let arr = new Array();
+main();
+
+function main() {
+    document.getElementById("com1").innerText =arr[arr.length-1];
+    document.getElementById("com2").innerText = arr[arr.length-2];
+    document.getElementById("com3").innerText = arr[arr.length-3];
+    document.getElementById("com4").innerText = arr[arr.length-4];
+    console.log(arr);    
 }
 
 
+
+function inputcom() {
+    x = document.getElementById("coment").value;
+    arr.push(x);
+}
+
+function inputname() {
+    x = document.getElementById("name").value;
+    alert('Привет ' + x);
+}
